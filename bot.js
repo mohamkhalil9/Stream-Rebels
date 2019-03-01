cf9991005db14c14bbe5d11994801d31bccdbfc0
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-01 10:10:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["415142691282616330"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
