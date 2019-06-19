14f3b9cdf73c41be5864e3a1c29af7f78a4fb171
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-19 04:36:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
+tconst Discord = require(‘discord.js’);
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const Canvas = require(‘canvas’);
+client.on(‘guildMemberAdd’, async member => {
+const channel = member.guild.channels.find(ch => ch.name === ‘member-log’);
+if (!channel) return;
+const canvas = Canvas.createCanvas(700, 250);
+const ctx = canvas.getContext('2d');
+// Since the image takes time to load, you should await it
+const background = await Canvas.loadImage('./wallpaper.jpg');
+// This uses the canvas dimensions to stretch the image onto the entire canvas
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+// Use helpful Attachment class structure to process the file for you
+const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
 
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const xkiller = member.guild.channels.find("name", "🙌『الـترحيب』-•");
-     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-  });
-});
+channel.send(`Welcome to the server, ${member}!`, attachment);
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
